eb0d43c1fa4df6cc715ece7694cb85c1f7a44e5f
Tony Lockhart <tlockhart6@gmail.com>
2021-04-11 22:19:09 -0400

    chore: updated product models

--- api/models/product.js
+++ api/models/product.js
@@ -12,7 +12,7 @@ module.exports = function (sequelize, DataTypes) {
         },
         productImage: {
             type: DataTypes.STRING,
-            unique: true,
+            unique: false,
             allowNull: false
         },
         cloudId: {
